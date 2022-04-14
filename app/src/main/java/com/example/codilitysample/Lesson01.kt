0e9aedbe2e86b4c14092c69c86d2d12ec1919851
AliDoran <alidoran@gmail.com>
2022-04-14 11:08:09 +0430

    Add FrogRiverOne

--- app/src/main/java/com/example/codilitysample/Lesson01.kt
+++ app/src/main/java/com/example/codilitysample/Lesson01.kt
@@ -1,51 +1,24 @@
 package com.example.codilitysample
 
-fun factorial(inputNumbers: Int): Int {
-    var i = 1
-    for (num in inputNumbers downTo 1)
-        i *= num
-    return i
-}
+//region BinaryGap
+/*
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
 
-fun typeStar(starCount: Int) {
-    for (star in 1..starCount) {
-        for (starPrint in 1..star) {
-            print('*')
-        }
-        print("\n")
-    }
-}
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
 
-fun typeStarSymmetricalUpToDown(starCount: Int) {
-    var spaceCount = 0
-    for (star in starCount downTo 1 step 2) {
-        for (space in 0..spaceCount)
-            print(' ')
-        for (starPrint in 1..star)
-            print('*')
-        spaceCount++
-        print("\n")
-    }
-}
+Write a function:
 
-fun fibonacci(limitedNumber: Int) {
-    var a = 0
-    var b = 1
-    while (b < limitedNumber) {
-        print("$b ")
-        b += a
-        a = b - a
-    }
-}
+class Solution { public int solution(int N); }
 
-fun printWeekDays() {
-    var days = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    for (day in days)
-        println(day)
-}
+that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
 
-class BinaryGap {
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
 
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..2,147,483,647].
+Copyright 2009–2022 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+ */
     private fun numToBinary(num: Double): String {
         var inputNum = num
         var binaryNum = ""
@@ -85,4 +58,4 @@ class BinaryGap {
             .map { it.length }
             .maxOrNull() ?:0
     }
-}
+//endregion
